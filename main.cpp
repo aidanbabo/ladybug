4fac1fe342a2ae17cbb581c85f261d19ed0b06fa
Aidan Barr Bono <aidan.babo@gmail.com>
2025-09-13 13:23:31 -0400

    encapsulate encryption status of an HTTP connection in a class

--- main.cpp
+++ main.cpp
@@ -35,89 +35,261 @@ std::string escape(std::string source) {
 	return output;
 }
 
-struct URL {
-	bool view_source = false;
-	std::string scheme;
-	std::string host;
-	uint16_t port;
-	std::string path;
+class HttpConnection {
+	int m_socket_fd;
+	SSL_CTX *m_ctx = nullptr;
+	SSL *m_ssl = nullptr;
 
+public:
+	HttpConnection(char const *host, uint16_t port, bool encrypt) {
+		addrinfo hints{};
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		std::string port_s = std::to_string(port);
+
+		addrinfo *res;
+		int status = getaddrinfo(host, port_s.c_str(), &hints, &res);
+		if (status != 0) {
+			std::cerr << gai_strerror(status) << std::endl;
+			assert(false);
+		}
+
+		m_socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (m_socket_fd == -1) {
+			perror("socket");
+			freeaddrinfo(res);
+			assert(false);
+		}
+
+		int connection_result = connect(m_socket_fd, res->ai_addr, res->ai_addrlen);
+		freeaddrinfo(res);
+		if (connection_result == -1) {
+			perror("connect");
+			close(m_socket_fd);
+			assert(false);
+		}
+
+		if (encrypt) {
+			const SSL_METHOD *method = TLS_client_method();
+			m_ctx = SSL_CTX_new(method);
+			if (!m_ctx) {
+				ERR_print_errors_fp(stderr);
+				close(m_socket_fd);
+				assert(false);
+			}
+
+			m_ssl = SSL_new(m_ctx);
+			if (!m_ssl) {
+				ERR_print_errors_fp(stderr);
+				SSL_CTX_free(m_ctx);
+				close(m_socket_fd);
+				assert(false);
+			}
+			if (SSL_set_fd(m_ssl, m_socket_fd) == 0) {
+				ERR_print_errors_fp(stderr);
+				SSL_free(m_ssl);
+				SSL_CTX_free(m_ctx);
+				close(m_socket_fd);
+				assert(false);
+			}
+
+			int connection_status = SSL_connect(m_ssl);
+			if (connection_status == 0) {
+				// gracefully failed
+				ERR_print_errors_fp(stderr);
+				SSL_free(m_ssl);
+				SSL_CTX_free(m_ctx);
+				close(m_socket_fd);
+				assert(false);
+			} else if (connection_status < 0) {
+				ERR_print_errors_fp(stderr);
+				SSL_free(m_ssl);
+				SSL_CTX_free(m_ctx);
+				close(m_socket_fd);
+				assert(false);
+			}
+		}
+	}
+
+	~HttpConnection() {
+		if (is_encrypted()) {
+			auto ret = SSL_shutdown(m_ssl);
+			if (ret < 0) {
+				//SSL_get_error(m_ssl, ret);
+				assert(false);
+			} else if (ret == 0) {
+				// no error, shutdown in progress
+			}
+			SSL_free(m_ssl);
+			SSL_CTX_free(m_ctx);
+		}
+		close(m_socket_fd);
+	}
+
+	bool is_encrypted() const {
+		return m_ssl != nullptr;
+	}
+
+	std::string request(std::string request) {
+		if (is_encrypted()) {
+			return request_with_tls(request);
+		} else {
+			return request_without_tls(request);
+		}
+	}
+
+private:
+	std::string request_with_tls(std::string request) const {
+
+		// there may be handleable failures here for larger request sizes?
+		int send_result = SSL_write(m_ssl, request.c_str(), request.size());
+		if (send_result <= 0) {
+			ERR_print_errors_fp(stderr);
+			SSL_shutdown(m_ssl);
+			SSL_free(m_ssl);
+			SSL_CTX_free(m_ctx);
+			close(m_socket_fd);
+			assert(false);
+		}
+
+		std::string response;
+		char buffer[1024];
+		int bytes_received;
+		// todo: remove infinite
+		for (;;) {
+			bytes_received = SSL_read(m_ssl, buffer, sizeof(buffer) - 1);
+			if (bytes_received <= 0) {
+				switch (SSL_get_error(m_ssl, bytes_received)) {
+				case SSL_ERROR_ZERO_RETURN:
+					goto after_read;
+				default:
+					// i can't use goto for error handling this is tragic?
+					// do i have to... use deconstructors?
+					ERR_print_errors_fp(stderr);
+					SSL_shutdown(m_ssl);
+					SSL_free(m_ssl);
+					SSL_CTX_free(m_ctx);
+					close(m_socket_fd);
+					assert(false);
+					break;
+				}
+			}
+			if (bytes_received == 0) {
+				break;
+			}
+
+			response.append(buffer, bytes_received);
+		}
+
+after_read:
+		return response;
+	}
+
+	std::string request_without_tls(std::string request) const {
+		int send_result = send(m_socket_fd, request.c_str(), request.size(), 0);
+		assert(send_result != -1);
+
+		std::string response;
+		char buffer[1024];
+		ssize_t bytes_received;
+		// todo: remove infinite
+		for (;;) {
+			bytes_received = recv(m_socket_fd, buffer, sizeof(buffer) - 1, 0);
+			assert(bytes_received != -1);
+			if (bytes_received == 0) {
+				break;
+			}
+
+			response.append(buffer, bytes_received);
+		}
+		return response;
+	}
+};
+
+class URL {
+	bool m_view_source = false;
+	std::string m_scheme;
+	std::string m_host;
+	uint16_t m_port;
+	std::string m_path;
+
+public:
 	URL(std::string_view url) {
 		auto n = url.find(":");
 		assert(n != std::string::npos);
-		this->scheme = url.substr(0, n);
+		m_scheme = url.substr(0, n);
 
-		if (this->scheme == "view-source") {
-			this->view_source = true;
+		if (m_scheme == "view-source") {
+			m_view_source = true;
 			url = url.substr(n + 1);
 
 			n = url.find(":");
 			assert(n != std::string::npos);
-			this->scheme = url.substr(0, n);
+			m_scheme = url.substr(0, n);
 		}
 
 		constexpr std::array supported_protocols{"http", "https", "file", "data"};
-		bool supported = std::find(supported_protocols.begin(), supported_protocols.end(), this->scheme) != supported_protocols.end();
+		bool supported = std::find(supported_protocols.begin(), supported_protocols.end(), m_scheme) != supported_protocols.end();
 		assert(supported);
-		if (this->scheme == "data") {
+		if (m_scheme == "data") {
 			url = url.substr(n + 1);
 		} else {
 			assert(url.substr(n + 1, 2) == "//");
 			url = url.substr(n + 3);
 		}
 
-		if (this->scheme == "data") {
+		if (m_scheme == "data") {
 			n = url.find(",");
 			assert(n != std::string::npos);
 			assert(url.substr(0, n) == "text/html");
 			// not really what this is for storing...
-			this->path = url.substr(n + 1);
+			m_path = url.substr(n + 1);
 			return;
 		}
 
 		n = url.find("/");
 		if (n == std::string::npos) {
-			this->host = url;
-			this->path = "/";
+			m_host = url;
+			m_path = "/";
 		} else {
-			this->host = url.substr(0, n);
-			this->path = url.substr(n);
+			m_host = url.substr(0, n);
+			m_path = url.substr(n);
 		}
 
-		n = this->host.find(":");
+		n = m_host.find(":");
 		if (n != std::string::npos) {
-			this->port = std::stoi(this->host.substr(n + 1));
-			this->host = this->host.substr(0, n);
-		} else if (this->scheme == "https") {
-			this->port = 443;
-		} else if (this->scheme == "http") {
-			this->port = 80;
-		} else if (this->scheme == "file") {
-			this->port = 0;
+			m_port = std::stoi(m_host.substr(n + 1));
+			m_host = m_host.substr(0, n);
+		} else if (m_scheme == "https") {
+			m_port = 443;
+		} else if (m_scheme == "http") {
+			m_port = 80;
+		} else if (m_scheme == "file") {
+			m_port = 0;
 		} else {
 			assert(false && "unreachable");
 		}
 
-		if (this->scheme == "file") {
-			assert(this->host == "");
-			assert(this->port == 0);
+		if (m_scheme == "file") {
+			assert(m_host == "");
+			assert(m_port == 0);
 		}
 	}
 
 	std::string request() const {
 
 		std::string response;
-		if (this->scheme == "file") {
-			response = this->load_file();
-		} else if (this->scheme == "data") {
-			response = this->path;
-		} else if (this->scheme == "http" || this->scheme == "https") {
-			response = this->request_http_generic();
+		if (m_scheme == "file") {
+			response = load_file();
+		} else if (m_scheme == "data") {
+			response = m_path;
+		} else if (m_scheme == "http" || m_scheme == "https") {
+			response = request_http();
 		} else {
 			assert(false);
 		}
 
-		if (this->view_source) {
+		if (m_view_source) {
 			return escape(response);
 		} else {
 			return response;
@@ -126,7 +298,7 @@ struct URL {
 
 private:
 	std::string load_file() const {
-		std::ifstream file(this->path);
+		std::ifstream file(m_path);
 		if (!file.is_open()) {
 			std::cerr << "Invalid path" << std::endl;
 			exit(1);
@@ -138,38 +310,22 @@ private:
 		return file_content;
 	}
 
-	std::string request_http_generic() const {
-		addrinfo hints{};
-		hints.ai_family = AF_INET;
-		hints.ai_socktype = SOCK_STREAM;
-		std::string port = std::to_string(this->port);
+	std::string request_http() const {
 
-		addrinfo *res;
-		int status = getaddrinfo(this->host.c_str(), port.c_str(), &hints, &res);
-		if (status != 0) {
-			std::cerr << gai_strerror(status) << std::endl;
+		bool encrypted;
+		if (m_scheme == "http") {
+			encrypted = false;
+		} else if (m_scheme == "https") {
+			encrypted = true;
+		} else {
 			assert(false);
 		}
+		HttpConnection connection = HttpConnection(m_host.c_str(), m_port, encrypted);
 
-		int socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-		if (socket_fd == -1) {
-			perror("socket");
-			freeaddrinfo(res);
-			assert(false);
-		}
-
-		int connection_result = connect(socket_fd, res->ai_addr, res->ai_addrlen);
-		freeaddrinfo(res);
-		if (connection_result == -1) {
-			perror("connect");
-			close(socket_fd);
-			assert(false);
-		}
-
-		std::string request = "GET " + this->path + " HTTP/1.1\r\n";
+		std::string request = "GET " + m_path + " HTTP/1.1\r\n";
 
 		std::vector<std::pair<std::string, std::string>> request_headers;
-		request_headers.push_back(std::make_pair("Host", this->host));
+		request_headers.push_back(std::make_pair("Host", m_host));
 		request_headers.push_back(std::make_pair("Connection", "close"));
 		request_headers.push_back(std::make_pair("User-Agent", "ladybug 1.0"));
 		for (auto pair : request_headers) {
@@ -180,17 +336,7 @@ private:
 		}
 		request.append("\r\n");
 
-		std::string response;
-		// todo: unified "connection" object to unify tls and non-tls
-		// ala Python
-		if (this->scheme == "http") {
-			response = this->request_http(socket_fd, request);
-		} else if (this->scheme == "https") {
-			response = this->request_https(socket_fd, request);
-		} else {
-			assert(false);
-		}
-		close(socket_fd);
+		std::string response = connection.request(request);
 
 		int status_line_end = response.find("\r\n");
 		assert(status_line_end != std::string::npos);
@@ -239,122 +385,31 @@ private:
 
 		return response;
 	}
-
-	std::string request_http(int socket_fd, std::string request) const {
-		int send_result = send(socket_fd, request.c_str(), request.size(), 0);
-		assert(send_result != -1);
-
-		std::string response;
-		char buffer[1024];
-		ssize_t bytes_received;
-		// todo: remove infinite
-		for (;;) {
-			bytes_received = recv(socket_fd, buffer, sizeof(buffer) - 1, 0);
-			assert(bytes_received != -1);
-			if (bytes_received == 0) {
-				break;
-			}
-
-			response.append(buffer, bytes_received);
-		}
-
-		return response;
-	}
-
-	std::string request_https(int socket_fd, std::string request) const {
-
-		const SSL_METHOD *method = TLS_client_method();
-		SSL_CTX *ctx = SSL_CTX_new(method);
-		if (!ctx) {
-			ERR_print_errors_fp(stderr);
-			close(socket_fd);
-			assert(false);
-		}
-
-		SSL *ssl = SSL_new(ctx);
-		if (!ssl) {
-			ERR_print_errors_fp(stderr);
-			SSL_CTX_free(ctx);
-			close(socket_fd);
-			assert(false);
-		}
-		if (SSL_set_fd(ssl, socket_fd) == 0) {
-			ERR_print_errors_fp(stderr);
-			SSL_free(ssl);
-			SSL_CTX_free(ctx);
-			close(socket_fd);
-			assert(false);
-		}
-
-		int connection_status = SSL_connect(ssl);
-		if (connection_status == 0) {
-			// gracefully failed
-			ERR_print_errors_fp(stderr);
-			SSL_free(ssl);
-			SSL_CTX_free(ctx);
-			close(socket_fd);
-			assert(false);
-		} else if (connection_status < 0) {
-			ERR_print_errors_fp(stderr);
-			SSL_free(ssl);
-			SSL_CTX_free(ctx);
-			close(socket_fd);
-			assert(false);
-		}
-
-		// there may be handleable failures here for larger request sizes?
-		int send_result = SSL_write(ssl, request.c_str(), request.size());
-		if (send_result <= 0) {
-			ERR_print_errors_fp(stderr);
-			SSL_shutdown(ssl);
-			SSL_free(ssl);
-			SSL_CTX_free(ctx);
-			close(socket_fd);
-			assert(false);
-		}
-
-		std::string response;
-		char buffer[1024];
-		int bytes_received;
-		// todo: remove infinite
-		for (;;) {
-			bytes_received = SSL_read(ssl, buffer, sizeof(buffer) - 1);
-			if (bytes_received <= 0) {
-				switch (SSL_get_error(ssl, bytes_received)) {
-				case SSL_ERROR_ZERO_RETURN:
-					goto after_read;
-				default:
-					// i can't use goto for error handling this is tragic?
-					// do i have to... use deconstructors?
-					ERR_print_errors_fp(stderr);
-					SSL_shutdown(ssl);
-					SSL_free(ssl);
-					SSL_CTX_free(ctx);
-					close(socket_fd);
-					assert(false);
-					break;
-				}
-			}
-			if (bytes_received == 0) {
-				break;
-			}
-
-			response.append(buffer, bytes_received);
-		}
-
-after_read:
-		auto ret = SSL_shutdown(ssl);
-		if (ret < 0) {
-			SSL_get_error(ssl, ret);
-		} else if (ret == 0) {
-			// no error, shutdown in progress
-		}
-		SSL_free(ssl);
-		SSL_CTX_free(ctx);
-
-		return response;
-	}
 };
+
+int unescape_sequence(std::string_view body, int i) {
+	assert(i + 2 < body.length());
+	char c2 = body[i];
+	char c3 = body[i + 1];
+	char c4 = body[i + 2];
+	if (c2 == 'l' && c3 == 't' && c4 == ';') {
+		std::cout << '<';
+		i += 3;
+	} else if (c2 == 'g' && c3 == 't' && c4 == ';') {
+		std::cout << '>';
+		i += 3;
+	} else {
+		assert(i + 3 < body.length());
+		char c5 = body[i + 3];
+		if (c2 == 'a' && c3 == 'm' && c4 == 'p' && c5 == ';') {
+			std::cout << '&';
+			i += 4;
+		} else {
+			assert(false);
+		}
+	}
+	return i;
+}
 
 void show(std::string_view body) {
 	int i = 0;
@@ -367,26 +422,7 @@ void show(std::string_view body) {
 			in_tag = false;
 		} else if (!in_tag) {
 			if (c == '&') {
-				assert(i + 2 < body.length());
-				char c2 = body[i];
-				char c3 = body[i + 1];
-				char c4 = body[i + 2];
-				if (c2 == 'l' && c3 == 't' && c4 == ';') {
-					std::cout << '<';
-					i += 3;
-				} else if (c2 == 'g' && c3 == 't' && c4 == ';') {
-					std::cout << '>';
-					i += 3;
-				} else {
-					assert(i + 3 < body.length());
-					char c5 = body[i + 3];
-					if (c2 == 'a' && c3 == 'm' && c4 == 'p' && c5 == ';') {
-						std::cout << '&';
-						i += 4;
-					} else {
-						assert(false);
-					}
-				}
+				i = unescape_sequence(body, i);
 			} else { 
 				std::cout << c;
 			}
